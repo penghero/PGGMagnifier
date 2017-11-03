7bff10708601455fc8882453ea629e8a4ec7129d
penghero <chen@chenpengs-MacBook-Air.local>
2017-11-03 10:50:27 +0800

    更新 加入github地址

--- PGGMagnifier/ViewController.m
+++ PGGMagnifier/ViewController.m
@@ -4,8 +4,9 @@
 //
 //  Created by 陈鹏 on 2017/11/3.
 //  Copyright © 2017年 penggege.CP. All rights reserved.
-//
-
+//使用中有问题 请前去GitHub与我联系 感谢您的使用
+//对您有帮助的话 送我一颗星星可好
+//GitHub地址 https://github.com/penghero/PGGMagnifier.git
 #import "ViewController.h"
 #import "PGGMagnigierView.h"
 
